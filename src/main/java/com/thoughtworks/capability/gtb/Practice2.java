b13621f791360564b41b6fbde908597adca39c48
EscapedSheep <sfgmdf001@126.com>
2020-09-17 18:05:27 +0800

    [maida] completed parctice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -11,6 +11,11 @@ import java.time.LocalDate;
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    int dayOfWeek = date.getDayOfWeek().getValue();
+    if (dayOfWeek <= 4) {
+      return date.plusDays(1);
+    }
+    int plusDays = 7 - dayOfWeek + 1;
+    return date.plusDays(plusDays);
   }
 }
